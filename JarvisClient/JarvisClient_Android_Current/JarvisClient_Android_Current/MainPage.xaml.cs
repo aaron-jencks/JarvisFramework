c5b1e566f7ec1078b14002c7b028c90c4ec1dcbb
iggy12345 <32805004+iggy12345@users.noreply.github.com>
2018-07-15 22:51:18 -0500

    Worked on App
    Created a way for threads to communicate with each other without needing to invoke methods

--- JarvisClient/JarvisClient_Android_Current/JarvisClient_Android_Current/MainPage.xaml.cs
+++ JarvisClient/JarvisClient_Android_Current/JarvisClient_Android_Current/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using aModuleClassLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,9 +10,29 @@ namespace JarvisClient_Android_Current
 {
 	public partial class MainPage : ContentPage
 	{
+        private CommObject CommServer;
+        private CommClient CommClient;
+
 		public MainPage()
 		{
 			InitializeComponent();
+
+            CommServer = new CommObject();
+
+            CommClient = new CommClient(CommServer);
+            CommClient.MsgRx += OnCommMsgRx;
+
+            commandBar.TextChanged += CommandBar_TextChanged;
 		}
+
+        private void CommandBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CommServer.GlobalEnQueue(new CommQueueMsg(commandBar.Text));
+        }
+
+        public void OnCommMsgRx(object sender, EventArgs e)
+        {
+            consoleOutput.Text += "Received a message\n";
+        }
 	}
 }
